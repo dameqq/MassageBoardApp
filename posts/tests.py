f9c6780040d09b1be4929fee18c0ed0cdeeeaf99
dameq <damianszymczak01@wp.pl>
2023-05-18 14:51:50 +0200

    first commit

---
+++ posts/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from .models import Post
+from django.urls import reverse
+# Create your tests here.
+
+class PostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="this is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "this is a test!")
+
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, "this is a test!")
